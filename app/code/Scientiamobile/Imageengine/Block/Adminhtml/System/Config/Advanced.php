1d6c181742bd0c18b665f40eb06b84c97176ab04
Jon Arne Sæterås <seteras@gmail.com>
2017-08-04 15:46:02 +0200

    initial commit

---
+++ app/code/Scientiamobile/Imageengine/Block/Adminhtml/System/Config/Advanced.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Scientiamobile\Imageengine\Block\Adminhtml\System\Config;
+
+class Advanced extends \Magento\Config\Block\System\Config\Form\Field
+{
+    /**
+     * Template path
+     *
+     * @var string
+     */
+    protected $_template = 'system/config/advance/document.phtml';
+
+    /**
+     * Render fieldset html
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     */
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $columns = $this->getRequest()->getParam('website') || $this->getRequest()->getParam('store') ? 5 : 4;
+        return $this->_decorateRowHtml($element, "<td colspan='{$columns}'>" . $this->toHtml() . '</td>');
+    }
+}
---
+++ app/code/Scientiamobile/Imageengine/registration.php
@@ -0,0 +1,7 @@
+<?php
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Scientiamobile_Imageengine',
+    __DIR__
+);
